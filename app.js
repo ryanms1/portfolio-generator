1589ad38dc67ff5ca530776c269adf6ddda4fe9c
unknown <ryan_shippley@yahoo.com>
2022-05-11 17:30:58 -0700

    captured user input

---
+++ app.js
@@ -0,0 +1,10 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = profileDataArr => {
+    
+    profileDataArr.forEach(profileItem => console.log(profileItem))
+};
+
+printProfileData(profileDataArgs)
+
+// 9.1.6
