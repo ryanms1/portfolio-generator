d03c3021d82352163614fc2c6245cd5a274ced6f
unknown <ryan_shippley@yahoo.com>
2022-06-03 18:32:22 -0700

    generated HTML file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template') 
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs
 
-const printProfileData = profileDataArr => {
-    
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-};
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-// 9.1.6
+  console.log('Portfolio complete! Check out index.html to see the output!')
+});
+
